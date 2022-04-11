0701f3493e9d3b5d082ae26ce292c0768bb32241
Oleh Baran <baran.oleg@gmail.com>
2022-04-12 00:23:30 +0300

    did tasks from block 3

--- block_3_tasks.js
+++ block_3_tasks.js
@@ -1,2 +1,23 @@
+const str = 'cat';
+const num = 5;
+const bool = true;
+
 // Task 1
-let one = 4;
+let res1 = str + bool;
+let res2 = str + num;
+let res3 = num + bool;
+
+// Task 2
+let res4 = str * bool;
+let res5 = str * num;
+let res6 = num * bool;
+
+// Task 3
+let res7 = str / bool;
+let res8 = str / num;
+let res9 = num / bool;
+
+// Task 4
+const value1 = Number(bool);
+const value2 = String(bool);
+const value3 = Boolean(num);
