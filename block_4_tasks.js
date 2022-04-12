9044a8ed19a4becc1aedfaff9c3140a8dff7babb
Oleh Baran <baran.oleg@gmail.com>
2022-04-12 16:06:23 +0300

    did tasks from block 4

--- block_4_tasks.js
+++ block_4_tasks.js
@@ -1,1 +1,43 @@
 // Task1
+let car = {
+    color: "black"
+};
+
+// Task2
+car.color = "green";
+
+// Task3
+function power() {
+    let engine = "320 hp";
+    return console.log(engine);
+}
+car.power = power();
+
+// Task4
+function CalcAmount(pears, apples) {
+    return pears + apples;
+}
+
+// Task5
+function CheckName(name) {
+    if (name == "Oleh") {
+        console.log(`Welcome ${name}`);
+    } else {
+        console.log("There is no such name");
+    }
+}
+
+// Task6
+function CheckType(arg) {
+    return console.log(typeof(arg));
+}
+
+// Task7
+function isPrime(num) {
+    for (let i = 2; i < num; i++){
+        if (num % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
